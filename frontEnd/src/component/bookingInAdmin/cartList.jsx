24b0d9b2c291249f3396b64ee4611d09d089d7bb
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-09-07 20:56:51 +0530

    cart added to  booking view in admin

---
+++ frontEnd/src/component/bookingInAdmin/cartList.jsx
@@ -0,0 +1,28 @@
+import CommonTable from "../common/table/table";
+
+export const CartList = (props) => {
+  return (
+    <CommonTable
+      dataList={[]}
+      tblRows={[
+        "Row No",
+        "Booking Id",
+        "Car_RegNo",
+        "Driver Nic",
+        "Car Type",
+        "Trip In KM",
+        "Extra KM Driven",
+        "Rental Type",
+        "Date of Pickup",
+        "Time of Pickup",
+        "Pickup Venue",
+        "Returned Date",
+        "Returned Time",
+        "Returned Venue",
+        "Damage Status",
+        "Loss Damage",
+        "Cost",
+      ]}
+    />
+  );
+};
